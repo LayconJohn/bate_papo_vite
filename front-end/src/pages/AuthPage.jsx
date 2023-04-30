b8b60621f0f048c5b811411d18b283bd713a6bf3
layconjohn <layconjohn@gmail.com>
2023-04-29 22:32:44 -0300

    feat: create auth page

---
+++ front-end/src/pages/AuthPage.jsx
@@ -0,0 +1,26 @@
+export default function AuthPage({ onAuth }) {
+    
+    function onSubmit(e) {
+        e.preventDefault();
+        const { value } = e.target[0];
+        onAuth({ username: value, secret: value });
+    };
+    
+    return (
+        <div className="background">
+        <form onSubmit={onSubmit} className="form-card">
+            <div className="form-title">Bem vindo(a) 👋</div>
+
+            <div className="form-subtitle">Crie um nome para começar</div>
+
+            <div className="auth">
+                <div className="auth-label">Usuário</div>
+                <input className="auth-input" name="username" />
+                <button className="auth-button" type="submit">
+                    Entrar
+                </button>
+            </div>
+        </form>
+        </div>
+    );
+}
