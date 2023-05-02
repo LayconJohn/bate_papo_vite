7c4c083951412aaec385d827e0ebcdb956d088bf
layconjohn <layconjohn@gmail.com>
2023-05-01 23:44:28 -0300

    style: Ajust in page chat

--- front-end/src/pages/ChatsPage.jsx
+++ front-end/src/pages/ChatsPage.jsx
@@ -3,11 +3,13 @@ import { PrettyChatWindow } from 'react-chat-engine-pretty';
 export default function ChatsPage({ user }) {
     
     return(
+      <div className="chatBoard" > 
         <PrettyChatWindow
-        projectId={"f8f71cbd-27ac-45fa-8a6e-bc988ddd9b89"}
-        username={user.username}
-        secret={user.secret}
-        style={{ height: '100vh' }}
-      />
+          projectId={"f8f71cbd-27ac-45fa-8a6e-bc988ddd9b89"}
+          username={user.username}
+          secret={user.secret}
+          style={{ height: '100vh' }}
+        />
+      </div>
     );
 };
